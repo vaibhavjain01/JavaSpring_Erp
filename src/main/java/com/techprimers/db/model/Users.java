1a4f89184a03d6c951fe80dfc7f841b8032c117b
Unknown <vaibhavjain01@hotmail.com>
2018-10-17 02:27:48 -0400

    Users

--- src/main/java/com/techprimers/db/model/Users.java
+++ src/main/java/com/techprimers/db/model/Users.java
@@ -9,48 +9,28 @@ import javax.persistence.Id;
 public class Users {
 
     @Id
-    @GeneratedValue
-    @Column(name = "id")
-    private Integer id;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "team_name")
-    private String teamName;
-    @Column(name = "salary")
-    private Integer salary;
+    //@GeneratedValue
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
 
     public Users() {
     }
 
-    public Integer getId() {
-        return id;
+    public String getUsername() {
+        return username;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeamName() {
-        return teamName;
-    }
-
-    public void setTeamName(String teamName) {
-        this.teamName = teamName;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
